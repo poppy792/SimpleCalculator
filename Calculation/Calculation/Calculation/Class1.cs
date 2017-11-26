1ef94ddb76b8677d562c25259da8cf1f78e205f3
Barbara <33121907+poppy792@users.noreply.github.com>
2017-11-26 14:30:27 +0100

    Add files via upload

---
+++ Calculation/Calculation/Calculation/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*Kreirati bibilioteku Calculation sa klasom Calculator čiji su funkcijski članovi sljedeći: Add() ,
+Subtract() , Multipy() , Divide() . Definirati svaku funkciju tako da ima 2 parametra i da vraća
+rezultat ovisno o operaciji.*/
+
+namespace Calculation
+{
+    public class Calculator
+    {
+        public float Add(float fPrviBroj, float fDrugiBroj)
+        {
+            return fPrviBroj + fDrugiBroj;
+        }
+        public float Substract(float fPrviBroj, float fDrugiBroj)
+        {
+            return fPrviBroj - fDrugiBroj;
+        }
+        public float Multiply(float fPrviBroj, float fDrugiBroj)
+        {
+            return fPrviBroj * fDrugiBroj;
+        }
+        public float Divide(float fPrviBroj, float fDrugiBroj)
+        {
+            return fPrviBroj / fDrugiBroj;
+        }
+    }
+}
---
+++ Calculation/Calculation/TestCalculationLib/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculation;
+
+namespace TestCalculationLib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Calculator Kalkulator = new Calculator();
+            Console.WriteLine("Unesite dva broja: ");
+            float fPrviBroj = Convert.ToSingle(Console.ReadLine());
+            float fDrugiBroj = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Odaberite jednu od ponuđenih operacija: ");
+            Console.WriteLine("1. Zbrajanje");
+            Console.WriteLine("2. Množenje");
+            Console.WriteLine("3. Oduzimanje");
+            Console.WriteLine("4. Dijeljenje");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            switch (a)
+            {
+                case 1:
+                    Console.WriteLine(fPrviBroj + "+" + fDrugiBroj + "="+ Kalkulator.Add(fPrviBroj, fDrugiBroj));
+                    break;
+                case 2:
+                    Console.WriteLine(fPrviBroj + "*" + fDrugiBroj + "=" + Kalkulator.Multiply(fPrviBroj, fDrugiBroj));
+                    break;
+                case 3:
+                    Console.WriteLine(fPrviBroj + "-" + fDrugiBroj + "=" + Kalkulator.Substract(fPrviBroj, fDrugiBroj));
+                    break;
+                case 4:
+                    Console.WriteLine(fPrviBroj + "/" + fDrugiBroj + "=" + Kalkulator.Divide(fPrviBroj, fDrugiBroj));
+                    break;
+                default:
+                    Console.WriteLine("Odabrali ste opciju koja ne postoji.");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
